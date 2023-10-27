497cb147c97ea44d98139a8a0ebebc06d55df19c
June <thuraaung.ai.mdy@gmail.com>
2023-10-27 14:35:56 +0700

    Update counter.rs

--- src/utils/counter.rs
+++ src/utils/counter.rs
@@ -32,3 +32,18 @@ pub fn counter(lines: Vec<String>) -> Counts {
         unique_word_count: unique_words.len(),
     }
 }
+
+#[test]
+fn test_counter() {
+    let input_lines = vec![
+        "This is a sample sentence.".to_string(),
+        "Another sentence for testing.".to_string(),
+        "This is a sample sentence.".to_string(),
+    ];
+
+    let counts = counter(input_lines);
+
+    assert_eq!(counts.word_count, 14);
+    assert_eq!(counts.line_count, 3);
+    assert_eq!(counts.unique_word_count, 9);
+}
