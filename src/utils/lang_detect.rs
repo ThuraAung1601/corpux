166195c527fe8b0afbe7cfb43fe4a92862b143d0
June <thuraaung.ai.mdy@gmail.com>
2023-10-27 16:39:22 +0700

    Update lang_detect.rs

--- src/utils/lang_detect.rs
+++ src/utils/lang_detect.rs
@@ -1,12 +1,3 @@
-// Ref: Kasitphoom Thowongs, "What Language is that?", Link: https://github.com/Kasitphoom/ELEMENTARY_SYSTEM_PROGRAMMING/blob/master/22.10.11/WhatLanguage_report_65011328.pdf
-// Modifications of P' Phoom's program made for the project:
-    // [1] Refactor as a utility function
-    // [2] Added 50 more languages + 1 for Unknown case
-    // [3] Solved panic cases for Unknown characters
-    // [4] Calculated the total numbers as well as the percentage of characters for each language included
-    // [5] Passed the information as a struct instead of printing out the results
-    // [6] Added unit test to verify the language detection function
-
 use std::collections::HashMap;
 
 #[derive(Debug, Clone, Copy, Eq, Hash, PartialEq)]
